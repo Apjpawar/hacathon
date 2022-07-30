bc1d0dd0c65082aabfcb2ccddb7d37bf53bf2625
Apjpawar <84678884+Apjpawar@users.noreply.github.com>
2022-07-30 10:24:48 +0530

    ui changes

--- src/components/imageUpload.js
+++ src/components/imageUpload.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import vector from '../images/Vectors.png';
 import axios from 'axios';
+import { AiOutlineCloudUpload } from "react-icons/ai";
+
 
 const ImageUpload = () => {
 
-    let [image, setImage] = useState('');
+    let [image, setImage] = useState();
     let [selectedImage, setSelectedImage] = useState(null);
 
 
@@ -15,50 +17,61 @@ const ImageUpload = () => {
     }
 
     const submit = () => {
-         const fd = new FormData();
-         fd.append('image', selectedImage, selectedImage.name);
-         axios.post('#',fd)
-            .then( res => {
+        const fd = new FormData();
+        fd.append('image', selectedImage, selectedImage.name);
+        axios.post('#', fd)
+            .then(res => {
                 console.log(res);
             });
         console.log(selectedImage);
     }
 
     return (
-        <div className='min-w-full min-h-screen '>
+        <div className=' min-w-full min-h-screen bg-[#093545] text-white'>
 
-            <div className=' text-left p-8 text-4xl'>
-                Demo
+            <div className='p-2 text-4xl shadow-4xl text-center bg-white text-orange-500'>
+                Wall Generator
             </div>
 
-            <div className='flex'>
-                <div className='border-2  w-[40%]'>
+            <div className='flex mt-28'>
+                <div className='border-2  w-[40%] '>
                     Instruction
                 </div>
 
-                <div className=' w-[60%] mx-auto border-2'>
-                    <div className='border-2 w-fit mx-auto min-w-[200px] min-h-[200px]'>
+                <div className=' w-[60%] mx-auto'>
+                    <div className='border-2 w-fit mx-auto min-w-[200px] min-h-[200px] '>
                         <img
-                            className='bg-cover min-w-[200px] max-w-[200px] min-h-[200px] max-h-[200px]'
+                            className='bg-cover min-w-[500px] max-w-[300px] min-h-[3npm `00px] max-h-[200px] '
                             src={image}
-                            
                         />
                     </div>
-                    <input
-                        className='w-56 h-16 mt-5'
-                        type='file'
-                        name='uploadedImage'
-                        onChange={handaleChange}
-                    />
-                    <button
-                        className=' px-10 py-1 bg-gray-300 rounded-md ml-10'
-                        type='submit'
-                        onClick={submit}
-                    >
-                        Send
-                    </button>
+                    <div className='flex gap-20 items-center justify-center border-2'>
+                        <label>Upload Image</label>
+                        <label for='fileUpload' className=''><AiOutlineCloudUpload className='w-12 h-16' /></label>
+
+
+                        <input
+                            style={{ color: 'red' }}
+                            className='w-56 h-16 mt-5'
+                            type='file'
+                            name='uploadedImage'
+                            onChange={handaleChange}
+                            id='fileUpload'
+                            hidden
+                        />
+
+                        <button
+                            className=' px-10 py-1 bg-orange-500 rounded-md ml-10'
+                            type='submit'
+                            onClick={submit}
+                        >
+                            Send
+                        </button>
+
+                    </div>
                 </div>
             </div>
+            <img className='min-w-full fixed bottom-0' src={vector} />
         </div>
     )
 }
